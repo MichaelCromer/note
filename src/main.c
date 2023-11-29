1b06aec9085d659d4aec7a47639a7fbe2aabb956
Michael Cromer <mccromer.g@gmail.com>
2023-11-29 16:43:45 +1100

    read lines from stdin in main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() 
 {
+  char* line = NULL;
+  size_t len = 0;
+  ssize_t read;
+
+  while ((read = getline(&line, &len, stdin)) != -1) 
+  {
+    printf("Got line: %s", line);
+  }
+
   return 0;
 }
